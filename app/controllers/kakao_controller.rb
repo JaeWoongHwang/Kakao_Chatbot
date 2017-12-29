7c10fdffd9cf3ce5cf317797ef058b15ad1d32f4
JaeWoongHwang <jwngh15@gmail.com>
2017-12-29 01:30:55 +0000

    Message function add

--- app/controllers/kakao_controller.rb
+++ app/controllers/kakao_controller.rb
@@ -8,5 +8,14 @@ class KakaoController < ApplicationController
   end
 
   def message
+    # 사용자가 보내준 텍스트를 그대로 말해주는 기능
+    user_message: params[:content]
+
+    return_message = {
+      :message => {
+          :text => user_message
+      }
+    }
+    render json: return_message
   end
 end
